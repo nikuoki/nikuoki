29645fa3eb0d9a2d08a443d9541fe35510320bcf
nikuoki <47143271+nikuoki@users.noreply.github.com>
2019-01-29 20:35:21 +0900

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+class Eto
+{
+    static void Main()
+    {
+        string[] eto = { "子", "丑", "寅", "卯", "辰", "巳", "午", "未", "申", "酉", "戌", "亥"};
+
+        Console.Write("西暦をを入力してください---");
+        int seireki = int.Parse(Console.ReadLine());
+        int num = (8 + seireki) % 12;       //干支の周期を西暦と合わせる
+
+        Console.WriteLine("{0}年の干支は{1}です", seireki, eto[num]);
+    }
+}
